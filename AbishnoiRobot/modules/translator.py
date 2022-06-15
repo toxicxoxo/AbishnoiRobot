b76f7cef04774035d3d053b9fc9a00e994af912b
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-15 20:29:24 +0530

    Update translator.py

--- AbishnoiRobot/modules/translator.py
+++ AbishnoiRobot/modules/translator.py
@@ -16,7 +16,7 @@ def totranslate(update: Update, context: CallbackContext) -> None:
             "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
             "Example: `/tr en-hi` to translate from English to Hindi\n"
             "Or use: `/tr en` for automatic detection and translating it into english.\n"
-            "Click here to see [List of available Language Codes](https://t.me/DevilsHeavenMF/148391).",
+            "Click here to see [List of available Language Codes](https://telegra.ph/ɪᴛs-ᴍᴇ-𒆜-Aʙɪsʜɴᴏɪ-06-15).",
             parse_mode="markdown",
             disable_web_page_preview=True,
         )
