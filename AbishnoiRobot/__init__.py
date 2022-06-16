9d0f07e2c3157e5e5a15f97271490a9155123165
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-16 13:22:45 +0530

    Update __init__.py

--- AbishnoiRobot/__init__.py
+++ AbishnoiRobot/__init__.py
@@ -7,6 +7,7 @@ import telegram.ext as tg
 from aiohttp import ClientSession
 from pyrogram import Client, errors
 from telethon import TelegramClient
+from Python_ARQ import ARQ
 
 StartTime = time.time()
 
