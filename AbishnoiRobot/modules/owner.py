3881547851051950b802c8530c3cf1ec39a000de
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-17 23:15:03 +0530

    Add files via upload

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -1,6 +1,22 @@
+# Code By Asad Deliver To Abɪsʜɴᴏɪ (ᴛʜᴀɴᴋs ᴀsᴀᴅ ᴅᴇʟɪᴠᴇʀ  )
+# © Alexa_Help
+
 from AbishnoiRobot import pbot as bot
-from pyrogram import filters 
-@bot.on_message(filters.command("owner"))
-def command(bot, message):  
-    bot.send_message(message.chat.id,"@Abishnoi1M")
-                     
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@app.on_message(filters.command("owner") & filters.private & ~filters.edited)
+async def useradd(_, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/9df5e00ded2ef88341769.jpg",
+        caption=f"""•ᴀʙɪsʜɴᴏɪ•""",
+   reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ", url=f"https://t.me/Abishnoi1M")
+                ]
+                
+           ]
+        ),
+    )
