fd0adbbf45def515126b874786494bf49f286882
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-17 23:20:32 +0530

    Update owner.py

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -5,7 +5,7 @@ from AbishnoiRobot import pbot as bot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-@app.on_message(filters.command("owner") & filters.private & ~filters.edited)
+@bot.on_message(filters.command("owner") & filters.private & ~filters.edited)
 async def useradd(_, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/9df5e00ded2ef88341769.jpg",
