679c0a9d6a23244ac864741a180155083490e673
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-18 00:28:44 +0530

    Add files via upload

---
+++ AbishnoiRobot/modules/owner.py
@@ -0,0 +1,31 @@
+# Code By Asad Deliver To Abɪsʜɴᴏɪ (ᴛʜᴀɴᴋs ᴀsᴀᴅ ᴅᴇʟɪᴠᴇʀ  )
+# © Alexa_Help
+
+  from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+  from AbishnoiRobot import pbot as bot
+  from pyrogram import filters
+
+
+   AK = "https://telegra.ph/file/348fd99cf32b44153f5c1.jpg"
+
+
+
+    @bot.on_message(filters.command(["owner", "noob"]))
+    async def owner(bot, message):
+    await message.reply_photo(
+          photo=AK,
+          caption=f"""**ʜᴇʏ {message.from_user.mention()},\n\nɪ ᴀᴍ [「 ᴀʙɢ 𒆜 ʀᴏʙᴏᴛ 」](t.me/Abishnoi_ro_bot)**
+
+
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="tg://user?id=1452219013",
+                  
+                     
+                    ),
+                ]
+            ]
+        ),
+    )
+
