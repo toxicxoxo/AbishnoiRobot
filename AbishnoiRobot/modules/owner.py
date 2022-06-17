23c342dc7a323a3be860cd1bf3bcc76a108c5bd9
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-17 22:25:28 +0530

    Update owner.py

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -3,19 +3,4 @@ from pyrogram import filters
 @bot.on_message(filters.command("owner"))
 def command(bot, message):  
     bot.send_message(message.chat.id,"@Abishnoi1M")
-                     reply_markupp=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="tg://user?id=1452219013"),
-                    InlineKeyboardButton(
-                        "• sᴏᴜʀᴄᴇ •",
-                        url="https://github.com/KingAbishnoi/AbishnoiRobot",
-                    ),
-                ]
-            ]
-        )
-        ")
-        
-   
-    
                      
