492ef0ff21b3bacb2c70a09f25aa375220b6fe11
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-18 00:44:23 +0530

    Update owner.py

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -1,32 +1,6 @@
-# Code By Asad Deliver To Abɪsʜɴᴏɪ (ᴛʜᴀɴᴋs ᴀsᴀᴅ ᴅᴇʟɪᴠᴇʀ  )
-# © Alexa_Help
+   from AbishnoiRobot import pbot as bot
+   from pyrogram import filters
 
-
-import asyncio
-from pyrogram import filters
-from pyrogram.types import (InlineKeyboardButton,                             InlineKeyboardMarkup, InputMediaPhoto, Message)
-from AbishnoiRobot import pbot as bot
-
-   AK = "https://telegra.ph/file/348fd99cf32b44153f5c1.jpg"
-
-
-
-    @bot.on_message(filters.command(["owner", "noob"]))
-    async def owner(bot, message):
-    await message.reply_photo(
-          photo=AK,
-          caption=f"""**ʜᴇʏ {message.from_user.mention()},\n\nɪ ᴀᴍ [「 ᴀʙɢ 𒆜 ʀᴏʙᴏᴛ 」](t.me/Abishnoi_ro_bot)**
-
-
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="tg://user?id=1452219013",
-                  
-                     
-                    ),
-                ]
-            ]
-        ),
-    )
-
+   @bot.on_message(filters.command("owner"))
+  def command(bot, message):
+    bot.send_message(message.chat.id, "[•ᴏᴡɴᴇʀ•](https://t.me/Abishnoi1M)")
