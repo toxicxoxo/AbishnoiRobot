ecf087249dee72024705bccb2cc711319d0b5cf9
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-18 01:02:23 +0530

    Delete owner.py

--- AbishnoiRobot/modules/owner.py
+++
@@ -1,6 +0,0 @@
-  from AbishnoiRobot import pbot as bot
-  from pyrogram import filters
-
-  @bot.on_message(filters.command("owner"))
-  def command(bot, message):
-     bot.send_message(message.chat.id, "[OWNER](https://t.me/Abishnoi1M)")
