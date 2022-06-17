0f30a6e4c65e639a342f8eda8bc902734cfca3f7
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-18 00:51:45 +0530

    Update owner.py

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -1,6 +1,6 @@
-   from AbishnoiRobot import pbot as bot
-   from pyrogram import filters
+  from AbishnoiRobot import pbot as bot
+  from pyrogram import filters
 
-   @bot.on_message(filters.command("owner"))
+  @bot.on_message(filters.command("owner"))
   def command(bot, message):
-    bot.send_message(message.chat.id, "[•ᴏᴡɴᴇʀ•](https://t.me/Abishnoi1M)")
+     bot.send_message(message.chat.id, "[OWNER](https://t.me/Abishnoi1M)")
