0659fe667a3721c4b752c4c2444c133e452a51fa
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-17 20:18:59 +0530

    Add files via upload

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -1,6 +1,19 @@
 from AbishnoiRobot import pbot as bot
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @bot.on_message(filters.command("owner"))
 def command(bot, message):
-    bot.send_message(message.chat.id, "[OWNER](https://t.me/Abishnoi1M)")
+    bot.send_message(message.chat.id,) 
+    
+    
+             reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="tg://user?id=1452219013"
+                    
+                                        ),
+                ]
+            ]
+        ),
+    )
