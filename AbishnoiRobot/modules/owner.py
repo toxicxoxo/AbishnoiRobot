9efb545d3cf2f13ceb5d0d8876fa104d0c196469
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-17 19:45:21 +0530

    Update owner.py

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -3,4 +3,4 @@ from pyrogram import filters
 
 @bot.on_message(filters.command("owner"))
 def command(bot, message):
-    bot.send_message(message.chat.id, "LINK [OWNER](https://t.me/Abishnoi1M)")
+    bot.send_message(message.chat.id, "[OWNER](https://t.me/Abishnoi1M)")
