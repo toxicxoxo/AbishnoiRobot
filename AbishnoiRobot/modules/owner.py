7ca928b88351353a8850b58f43a33cb2a5d09736
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-17 20:44:57 +0530

    Update owner.py

--- AbishnoiRobot/modules/owner.py
+++ AbishnoiRobot/modules/owner.py
@@ -4,17 +4,4 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @bot.on_message(filters.command("owner"))
 def command(bot, message):
-    bot.send_message(message.chat.id,
-                     
-    buttons = [
-          [
-             reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="https://t.me/Abishnoi1M"
-                    
-                                        ),
-                ]
-            ]
-        ),
-    )
+    bot.send_message(message.chat.id, " [OWNER](button url://t.me/Abishnoi1M)")
