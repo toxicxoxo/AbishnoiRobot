cedc6abe01834d1bb6a5f68f6be42f8592d8b836
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-18 10:47:12 +0530

    Update noob.py

--- AbishnoiRobot/modules/noob.py
+++ AbishnoiRobot/modules/noob.py
@@ -1,6 +1,6 @@
-from AbishnoiRobot import pbot as bot
-from pyrogram import filters
-
-@bot.on_message(filters.command("owner"))
-def command(bot, message):
-    bot.send_message(message.chat.id, "LINK [OWNER](https://t.me/Abishnoi1M)")
+      from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton   from pyrogram import filters   from AbishnoiRobot import pbot as client    
+ABISHNOI = "https://telegra.ph/file/7bd111132fce009e4605e.jpg"  
+@client.on_message(filters.command(["noob", "owner"]))
+async def repo(client, message):     await message.reply_photo(         photo=ABISHNOI,         caption=f"""**ʜᴇʏ {message.from_user.mention()},\n\nɪ ᴀᴍ [「 ᴀʙɢ 𒆜 ʀᴏʙᴏᴛ 」](t.me/Abishnoi_ro_bot)**
+""",         reply_markup=InlineKeyboardMarkup(             [                 [                     InlineKeyboardButton("• ᴏᴡɴᴇʀ •", url="tg://user?id=1452219013"),                     InlineKeyboardButton(                         "• sᴏᴜʀᴄᴇ •",                         url="https://github.com/KingAbishnoi/AbishnoiRobot",                     ),                 ]             ]         ),     )  
+__mod_name__ = "Oᴡɴᴇʀ" 
