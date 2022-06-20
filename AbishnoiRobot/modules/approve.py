6dc3e6ea8d5229e9e0d3a199e7012764cb223474
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-21 00:10:54 +0530

    Update approve.py

--- AbishnoiRobot/modules/approve.py
+++ AbishnoiRobot/modules/approve.py
@@ -211,7 +211,7 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Aᴘᴘʀᴏᴠᴇ"
+__mod_name__ = "Aᴘᴘʀᴏᴠᴇmain"
 
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
