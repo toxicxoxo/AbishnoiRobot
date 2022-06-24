8e1a878ae26feef8ea305cbb74055eec574973a3
ɪᴛ's ᴍᴇ ᴀʙɪsʜɴᴏɪ <106365712+KingAbishnoi@users.noreply.github.com>
2022-06-25 01:02:40 +0530

    Update afk_mongo.py

--- AbishnoiRobot/helper_extra/afk_mongo.py
+++ AbishnoiRobot/helper_extra/afk_mongo.py
@@ -26,6 +26,6 @@ async def get_afk_users() -> list:
         return []
     users_list = []
     for user in users.to_list(length=1000000000):
-        users_list.append(user) 
+        users_list.append(user)
     return users_list
 
