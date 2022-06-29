ee8525af750c5ff46d9e7d062f962abef660ffac
toxicxoxo <106758630+toxicxoxo@users.noreply.github.com>
2022-06-29 15:32:23 +0530

    Update ping.py

--- AbishnoiRobot/modules/ping.py
+++ AbishnoiRobot/modules/ping.py
@@ -80,7 +80,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        " ❣︎ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! ❣︎\n"
+        " ❣︎ ɪ ᴀᴍ ᴀʟɪᴠᴇ HOMIES! ❣︎\n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
