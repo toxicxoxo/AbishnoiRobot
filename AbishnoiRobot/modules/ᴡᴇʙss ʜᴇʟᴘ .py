15d0dd3c7ec0d899dba473213c55b6f4c67e2785
toxicxoxo <106758630+toxicxoxo@users.noreply.github.com>
2022-06-29 14:11:31 +0530

    Update ᴡᴇʙss ʜᴇʟᴘ .py

--- AbishnoiRobot/modules/ᴡᴇʙss ʜᴇʟᴘ .py
+++ AbishnoiRobot/modules/ᴡᴇʙss ʜᴇʟᴘ .py
@@ -1,9 +1,9 @@
 __mod_name__ = "ᴡᴇʙss 📥"
 
 __help__ = """
-➥ /webss *:* ɢᴇᴛ ᴇɴᴛᴇʀғᴀᴄᴇ  ᴀɴʏ ᴡᴇʙ 
+🌟 /webss *:* ɢᴇᴛ ᴇɴᴛᴇʀғᴀᴄᴇ  ᴀɴʏ ᴡᴇʙ 
     ᴇx = `/webss xnxx.com `
     
-𒊹︎︎︎ ᴘᴏᴡᴇʀᴅ ʙʏ @Abishnoi1M
+🌟 THANKS WINDFLOWER 🌟
 
 """
