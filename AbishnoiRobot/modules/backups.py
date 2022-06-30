a6924c3013360678d67e9bc7e70ec88fb8702864
toxicxoxo <106758630+toxicxoxo@users.noreply.github.com>
2022-06-30 13:17:15 +0530

    Update backups.py

--- AbishnoiRobot/modules/backups.py
+++ AbishnoiRobot/modules/backups.py
@@ -342,7 +342,7 @@ def export_data(update, context):
     context.bot.sendDocument(
         current_chat_id,
         document=open("AbishnoiRobot{}.backup".format(chat_id), "rb"),
-        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `AbishnoiRobot-Backup` was specially made for notes 📚.".format(
+        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `HOMIES BOT-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
